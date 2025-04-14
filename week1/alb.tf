3e514adf7b785196bcbf6b24d765e4d5adf22543
Arsenii Shynkaruk <arseniishynkaruk@gmail.com>
2025-04-14 08:00:06 +0000

    replace alb module with aws resources

--- week1/alb.tf
+++ week1/alb.tf
@@ -9,82 +9,121 @@ data "aws_route53_zone" "this" {
   private_zone = false
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+resource "aws_security_group" "alb_sg" {
+  name        = "${local.base_tag}-alb-sg"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.vpc.id
 
-  name    = "my-alb"
-  vpc_id  = aws_vpc.vpc.id
-  subnets = [for subnet in aws_subnet.lb_subnet : subnet.id]
-
-  security_group_ingress_rules = {
-    all_http = {
-      from_port   = 80
-      to_port     = 80
-      ip_protocol = "tcp"
-      description = "HTTP web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-    all_https = {
-      from_port   = 443
-      to_port     = 443
-      ip_protocol = "tcp"
-      description = "HTTPS web traffic"
-      cidr_ipv4   = "0.0.0.0/0"
-    }
-  }
-  security_group_egress_rules = {
-    all = {
-      ip_protocol = "-1"
-      cidr_ipv4   = "10.0.0.0/16"
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP web traffic"
   }
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-https = {
-      port            = 443
-      protocol        = "HTTPS"
-      certificate_arn = data.aws_acm_certificate.this.arn
-      ssl_policy      = "ELBSecurityPolicy-2016-08"
-
-      forward = {
-        target_group_key = "instance"
-      }
-    }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS web traffic"
   }
 
-  target_groups = {
-    instance = {
-      name_prefix = "h1"
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
-      target_id   = "i-0f6d38a07d50d080f"
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
-  route53_records = {
-    alb = {
-      zone_id = data.aws_route53_zone.this.zone_id
-      name    = data.aws_route53_zone.this.name
-      type    = "A"
-      alias = {
-        name                   = data.aws_route53_zone.this.name
-        zone_id                = data.aws_route53_zone.this.zone_id
-        evaluate_target_health = true
-      }
-    }
+  tags = {
+    Name = "${local.base_tag}-alb-sg"
   }
+}
+
+resource "aws_lb" "alb" {
+  name               = "${local.base_tag}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [for subnet in aws_subnet.lb_subnet : subnet.id]
+
+  enable_deletion_protection = false
 
   tags = {
     Name = "${local.base_tag}-alb"
   }
 }
+
+resource "aws_lb_target_group" "asg" {
+  name_prefix = "h1"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.vpc.id
+  target_type = "instance"
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200-299"
+  }
+
+  tags = {
+    Name = "${local.base_tag}-tg"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = data.aws_acm_certificate.this.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  lb_target_group_arn    = aws_lb_target_group.asg.arn
+}
+
+resource "aws_route53_record" "alb" {
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = data.aws_route53_zone.this.name
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.alb.dns_name
+    zone_id                = aws_lb.alb.zone_id
+    evaluate_target_health = true
+  }
+}
+

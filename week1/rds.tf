64046631bc93584391519d0e296ec1ff29095fb2
Arsenii Shynkaruk <arseniishynkaruk@gmail.com>
2025-04-14 07:58:42 +0000

    set up rds credentials

--- week1/rds.tf
+++ week1/rds.tf
@@ -1,3 +1,7 @@
+resource "random_password" "password" {
+  length = 8
+}
+
 resource "aws_security_group" "rds_sg" {
   name   = "${local.base_tag}-rds-sg"
   vpc_id = aws_vpc.vpc.id
@@ -19,16 +23,18 @@ module "rds" {
 
   identifier = "${local.base_tag}-rds"
 
-  engine            = "mysql"
-  family            = "mysql5.7"
-  engine_version    = "5.7"
-  instance_class    = var.db_instance_type
-  allocated_storage = var.allocated_storage
+  engine               = "mysql"
+  family               = "mysql8.0"
+  engine_version       = "8.0"
+  major_engine_version = "8.0"
+  instance_class       = var.db_instance_type
+  allocated_storage    = var.allocated_storage
 
-  #   db_name  = "demodb"
-  #   username = "user"
-  #   port     = "3306"
-
+  db_name                     = var.db_name
+  username                    = var.db_username
+  password                    = random_password.password.result
+  port                        = 3306
+  manage_master_user_password = false
   tags = {
     Name = "${local.base_tag}-rds"
   }
@@ -36,7 +42,8 @@ module "rds" {
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
   multi_az                = true
-  create_db_subnet_group  = true
-  subnet_ids              = [for subnet in aws_subnet.rds_subnet : subnet.id]
+  create_db_subnet_group  = false
+  db_subnet_group_name    = aws_db_subnet_group.this.name
   backup_retention_period = 3
+  skip_final_snapshot     = true
 }

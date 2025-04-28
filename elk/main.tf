a688af9c1c0a1867b44bfe893450979faf88401c
Arsenii Shynkaruk <arseniishynkaruk@gmail.com>
2025-04-28 19:15:40 +0000

    feature: fetch ami from data

--- elk/main.tf
+++ elk/main.tf
@@ -1,3 +1,13 @@
+data "aws_ami" "al2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_security_group" "kibana_sg" {
   vpc_id = aws_vpc.vpc.id
   name   = "${var.base_tag}-kibana-sg"
@@ -19,35 +29,131 @@ resource "aws_security_group" "kibana_sg" {
   }
 }
 
-resource "aws_instance" "name" {
+resource "aws_instance" "es-master" {
   security_groups = [aws_security_group.kibana_sg.id]
-
-  subnet_id     = aws_subnet.kibana_subnet.id
-  instance_type = var.instance_type
-  ami           = var.ami_id
-  user_data     = file("./elk.sh")
-
+  key_name        = var.key_name
+  subnet_id       = aws_subnet.kibana_subnet.id
+  instance_type   = var.instance_type
+  ami             = data.aws_ami.al2.id
+  user_data       = file("./es-master.sh")
   tags = {
-    Name = "${var.base_tag}-kibana"
+    Name = "${var.base_tag}-es-master"
   }
-
 }
 
-# data "aws_route53_zone" "this" {
-#   name         = var.domain_name
-#   private_zone = false
+resource "aws_instance" "es-data" {
+  security_groups = [aws_security_group.kibana_sg.id]
+  key_name        = var.key_name
+  subnet_id       = aws_subnet.kibana_subnet.id
+  instance_type   = var.instance_type
+  ami             = data.aws_ami.al2.id
+  user_data       = file("./es-data.sh")
+  tags = {
+    Name = "${var.base_tag}-es-data"
+  }
+}
+
+module "alb-kibana" {
+  source       = "../modules/alb"
+  user_data    = base64encode(file("./kibana.sh"))
+  alb_base_tag = var.base_tag
+  vpc_id       = aws_vpc.vpc.id
+  gateway_id   = aws_internet_gateway.igw.id
+  alb_dns_name = "kibana.${var.domain_name}"
+  port         = 80
+}
+
+# resource "aws_instance" "kibana" {
+#   depends_on      = [aws_instance.es-data, aws_instance.es-master]
+#   security_groups = [aws_security_group.kibana_sg.id]
+#   key_name        = var.key_name
+#   subnet_id       = aws_subnet.kibana_subnet.id
+#   instance_type   = var.kibana_instance_type
+#   ami             = data.aws_ami.al2.id
+#   user_data       = file("./kibana.sh")
+#   tags = {
+#     Name = "${var.base_tag}-kibana"
+#   }
 # }
 
-# resource "aws_eip" "kibana_eip" {
-#   instance = aws_instance.name.id
+resource "aws_instance" "logstash" {
+  depends_on      = [aws_instance.es-data, aws_instance.es-master]
+  security_groups = [aws_security_group.kibana_sg.id]
+  key_name        = "key"
+  subnet_id       = aws_subnet.kibana_subnet.id
+  instance_type   = var.instance_type
+  ami             = data.aws_ami.al2.id
+  user_data       = file("./logstash.sh")
+  tags = {
+    Name = "${var.base_tag}-logstash"
+  }
+}
 
-# }
+resource "aws_instance" "filebeat" {
+  security_groups = [aws_security_group.kibana_sg.id]
+  key_name        = "key"
+  subnet_id       = aws_subnet.kibana_subnet.id
+  instance_type   = var.instance_type
+  ami             = data.aws_ami.al2.id
+  user_data       = file("filebeat.sh")
+
+  tags = {
+    Name = "${var.base_tag}-filebeat"
+  }
+}
+
+data "aws_route53_zone" "this" {
+  name         = var.domain_name
+  private_zone = false
+}
+
+resource "aws_route53_zone" "elk" {
+  name = var.local_domain_name
+  vpc {
+    vpc_id = aws_vpc.vpc.id
+  }
+}
 
 # resource "aws_route53_record" "kibana" {
 #   zone_id = data.aws_route53_zone.this.zone_id
-#   name    = "elk.${data.aws_route53_zone.this.name}"
+#   name    = "kibana.${var.domain_name}"
+#   ttl     = 300
 #   type    = "A"
-#   records = [aws_eip.kibana_eip.public_ip]
+#   records = [aws_instance.kibana.public_ip]
 # }
+
+resource "aws_route53_record" "es-master" {
+  zone_id = aws_route53_zone.elk.zone_id
+  name    = "es-master.${var.local_domain_name}"
+  ttl     = 300
+  type    = "A"
+  records = [aws_instance.es-master.private_ip]
+}
+
+resource "aws_route53_record" "es-data" {
+  zone_id = aws_route53_zone.elk.zone_id
+  name    = "es-data.${var.local_domain_name}"
+  ttl     = 300
+  type    = "A"
+  records = [aws_instance.es-data.private_ip]
+}
+
+resource "aws_route53_record" "filebeat" {
+  zone_id = data.aws_route53_zone.this.zone_id
+  name    = "filebeat.${var.domain_name}"
+  ttl     = 300
+  type    = "A"
+  records = [aws_instance.filebeat.public_ip]
+}
+
+resource "aws_route53_record" "logstash" {
+  zone_id = aws_route53_zone.elk.zone_id
+  name    = "logstash.${var.local_domain_name}"
+  ttl     = 300
+  type    = "A"
+  records = [aws_instance.logstash.private_ip]
+}
+
+
 
 

93d38863361f9e1a6bfd89de1c23a0c964e15a82
Arsenii Shynkaruk <arseniishynkaruk@gmail.com>
2025-04-28 19:13:16 +0000

    fix typo in filebeat config

--- elk/filebeat.sh
+++ elk/filebeat.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 yum update -y
 amazon-linux-extras install nginx1 -y
-systemctl enable nginx
-systemctl start nginx
 
 rpm --install https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.17.8-x86_64.rpm
 
-echo "
+cat > /etc/filebeat/filebeat.yml << "EOF"
+filebeat.inputs:
+- type: log
+  enabled: true
+  paths:
+    - /var/log/nginx/access.log
 setup.template.enabled: false
 filebeat.config.modules:
   path: ${path.config}/modules.d/*.yml
   reload.enabled: true
-
 filebeat.modules:
 - module: nginx
   access:
     enabled: true
-
 output.logstash:
   enabled: true
   hosts: ["logstash.elk.internal:5044"]
-" > /etc/filebeat/filebeat.yml
+EOF
 
+
+systemctl enable nginx
+systemctl start nginx
 filebeat modules enable nginx
 systemctl enable filebeat
-systemctl filebeat start
+systemctl start filebeat
 

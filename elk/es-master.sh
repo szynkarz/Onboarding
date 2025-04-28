9584ea83a5485f2baaeda1be6b4fafeb0fa256f3
Arsenii Shynkaruk <arseniishynkaruk@gmail.com>
2025-04-28 08:31:09 +0000

    add elasticsearch config

---
+++ elk/es-master.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+yum update -y
+rpm --install https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.17.8-x86_64.rpm
+
+echo "
+cluster.name: elk-cluster
+node.name: master
+bootstrap.memory_lock: true
+network.host: "0.0.0.0"
+discovery.seed_hosts: ["es-master.elk.internal", "es-data.elk.internal"]
+cluster.initial_master_nodes: ["master"]
+
+xpack.security.enabled: false
+" >> /etc/elasticsearch/elasticsearch.yml
+
+systemctl enable elasticsearch
+systemctl start elasticsearch

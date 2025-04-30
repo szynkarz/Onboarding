a82bb4e6b36e31921bdf7cc22847beefb8727770
Arsenii Shynkaruk <arseniishynkaruk@gmail.com>
2025-04-30 12:44:20 +0000

    refactor providers

--- lambda/provider.tf
+++ lambda/provider.tf
@@ -4,10 +4,14 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
   }
 }
 
 provider "aws" {
-  region = "eu-central-1"
+  region = var.region
 }
 
